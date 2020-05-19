c6a47018f11782ec55ba83733568bda217e23d34
Gabriel Decilio <gdecilio@orion.tech>
2020-05-19 18:12:23 -0300

    criação do backend e configuração

---
+++ backend/src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose.connect("mongodb://localhost/mymoney", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
